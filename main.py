8afebb2fbaa64dc3d53003ab6d64024715c738c1
Edwin <ecarra7979@gmail.com>
2016-01-10 23:23:34 -0500

    changed hash_table to main.py; still working on flags...

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+from number_gen import hash_key
+import sys, getopt
+
+D = {}
+def main(argv):
+    try:
+	opts, args = getopt.getopt(argv, "h:m:p")
+    except getopt.GetoptError:
+        print 'Error: Type main.py -h for help.'
+	sys.exit(2)
+    for opt, arg in opts:
+	if opt == '-h':
+	    print 'main.py -m <"message"> -p <key>'
+	    sys.exit()
+	elif opt == '-m':
+
+            #capture a str of txt and issue a hash_key
+            hide_text = raw_input("Enter a message: ")
+            D[hash_key] = hide_text
+            #write to file
+
+	elif opt == 'p':
+	    #print to screen and pull with key
+            for k,v in D.items():
+                if k == hide_text:
+	            print "Your Message: " + v
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
