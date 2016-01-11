5b25408ced078fdecb22b9f9ae646722793d942a
ecarrasquillo <ecarrasquillo@wayfair.com>
2016-01-11 15:01:51 -0800

    added json dump and now trying to add json load with a for loop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from number_gen import hash_key
-import sys, getopt
+import sys, getopt, json
 
 #version = 1.0.0
 #output_filename = 'main.py'
@@ -8,7 +8,7 @@ import sys, getopt
 D = {}
 def main(argv):
     try:
-	options, remainder  = getopt.getopt(argv, "mp:", ['message=','pull'])
+	options, remainder  = getopt.getopt(argv, "mp", ['message','pull'])
     except getopt.GetoptError:
         print 'Error: Type main.py -h for help.'
 	sys.exit(2)
@@ -17,14 +17,20 @@ def main(argv):
             #capture a str of txt and issue a hash_key
             hide_text = raw_input("Enter a message: ")
             D[hash_key] = hide_text
-            print D
+            print D.split('.')
             #write to file
+	    json.dump(D, open("file.txt", 'w'))
 
 	elif opt in ('-p', '--pull'):
+	    search_key = raw_input("Enter a key: ")
 	    #print to screen and pull with key
-            for k,v in D.items():
-                if k == hide_text:
-	            print "Your Message: " + v
+		
+            for obj in json.loads(open("file.txt")):
+		print obj
+                h_key = obj['hash_key'].split('.')[0]
+		m_val = obj['hide_text']
+		if search_key == h_key:
+	            print "Your Message: " + m_val
 	else:
             print 'main.py -m <"message"> -p <key>'
             sys.exit()
