e6be9e3cf5c52082851684fb9b21a523b3a6cb4f
Edwin <ecarra7979@gmail.com>
2016-01-10 23:35:43 -0500

    changed the else to elif for the p flag

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ import sys, getopt
 D = {}
 def main(argv):
     try:
-	opts, args = getopt.getopt(argv, "h:m:p")
+	opts, args = getopt.getopt(argv, "m:p")
     except getopt.GetoptError:
         print 'Error: Type main.py -h for help.'
 	sys.exit(2)
     for opt, arg in opts:
-	if opt == '-h':
-	    print 'main.py -m <"message"> -p <key>'
-	    sys.exit()
-	elif opt == '-m':
-
+	if opt == '-m':
             #capture a str of txt and issue a hash_key
             hide_text = raw_input("Enter a message: ")
             D[hash_key] = hide_text
@@ -25,6 +21,9 @@ def main(argv):
             for k,v in D.items():
                 if k == hide_text:
 	            print "Your Message: " + v
+	else:
+            print 'main.py -m <"message"> -p <key>'
+            sys.exit()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
