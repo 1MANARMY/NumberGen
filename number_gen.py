cdaade198e2bae8a13192818391f783ba783e44c
Edwin <ecarra7979@gmail.com>
2016-01-10 21:27:08 -0500

    simplified the number generator, and join the numbers correctly

--- number_gen.py
+++ number_gen.py
@@ -1,10 +1,11 @@
 import random
+
 num_list = []
 def random_number():
-    # returns 10 numbers between 1 and 10
+    # returns 10 numbers between 0 and 9
     for i in xrange(10):
-        yield random.randint(1,10)
+        yield random.randint(0,9)
 
 for n in random_number():
-    num_list.insert(0, n)
-print num_list
+    var = num_list.insert(0, n)
+print ''.join(str(n) for n in num_list)
