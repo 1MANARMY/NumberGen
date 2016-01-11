25ae85aa31f9a873b17a68be990a6ca2dcef295d
Edwin <ecarra7979@gmail.com>
2016-01-11 00:03:34 -0500

    got the args to work and I am now able to add a message and have it printed

--- main.py
+++ main.py
@@ -2,21 +2,25 @@
 from number_gen import hash_key
 import sys, getopt
 
+#version = 1.0.0
+#output_filename = 'main.py'
+
 D = {}
 def main(argv):
     try:
-	opts, args = getopt.getopt(argv, "m:p")
+	options, remainder  = getopt.getopt(argv, "mp:", ['message=','pull'])
     except getopt.GetoptError:
         print 'Error: Type main.py -h for help.'
 	sys.exit(2)
-    for opt, arg in opts:
-	if opt == '-m':
+    for opt, arg in options:
+	if opt in ('-m', '--message'):
             #capture a str of txt and issue a hash_key
             hide_text = raw_input("Enter a message: ")
             D[hash_key] = hide_text
+            print D
             #write to file
 
-	elif opt == 'p':
+	elif opt in ('-p', '--pull'):
 	    #print to screen and pull with key
             for k,v in D.items():
                 if k == hide_text:
